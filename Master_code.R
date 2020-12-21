88b63bfca56ea843ff7bd50759fb60fc66242d5e
Elliot-JG <ejg27@student.le.ac.uk>
2020-12-21 15:01:51 +0000

    Normal distribution tests

--- Master_code.R
+++ Master_code.R
@@ -51,9 +51,78 @@ wellingborough <- complete_OAC %>%
 # k041 Households with two or more cars or vans
 # k046 Employed persons aged between 16 and 74 who work part-time
 
+# Select the above columns from the wellingborough dataset
 wellingborough_stats <- wellingborough %>%
   dplyr::select(k004, k009, k010, k027, k031, k041, k046) %>%
+  # Basis and desc set statistics
+  # These stats are 'per output area', across the many output areas that make up Wellingborough
+  # The average of k046 for example, is not the average age. It is the average number of 
+  # Employed persons aged between 16 and 74 who work part-time, for a given output area 
   pastecs::stat.desc()
 
 # Very basic initial first analysis, but it doesn't show us a lot and it's difficult to read
-# Also some of these just aren't very useful (do we really need to know the sum of any of these variablea?)
+
+# Because this is only a 'sample' (not 100% of people were surveyed, though it may be very close)
+# we might be particulary interested in the sample statistics
+  # SE.mean = The variability of the mean calculated in different samples (central limit theorem)
+  # cI.mean = There is a 95% probability that the mean of the whole population lies within here somewhere
+
+# pick out most interesting stats here 
+  # Some of the sums
+
+# Lets explore if the people who work part time are normally distributed or skewed
+# This is a good example because we already know the answer
+  # Hopefully, 16 year olds will be working more part time than 74 years old 
+  # OACs with a more youthful population may skew the data, as more households in these OAC's will have people working 
+
+# Histogram 
+wellingborough %>%
+  ggplot2::ggplot(
+    aes(x = k046))+
+  ggplot2::geom_histogram(binwidth = 2)
+  
+wellingborough %>%
+  ggplot2::ggplot(aes(x = k046))+
+  ggplot2::geom_bar()+
+  scale_x_binned(n.breaks = 15)
+
+# Looks like a normal distribution, but can investigate further 
+
+# Comparison to Normal distribution 
+wellingborough %>%
+  ggplot2::ggplot(aes(sample = k046))+
+  ggplot2::stat_qq()+
+  ggplot2::stat_qq_line()
+
+# Ok, interesting. It's closer than I thought 
+# Throughout the data there is a bigger proportion of OAC's that have a particulary 
+# large and small number of households that have people between 16-74 working part-time, than otherwise should for a
+# normal distribution 
+# Also be aware that this data shows one OAC with a particulary high number of people working part time  
+
+# Characteristics of a data set
+  # A normal distribution test
+
+wellingborough %>%
+dplyr::pull(k046)%>%
+stats::shapiro.test()
+
+# The probability of this data being normally distributed is < 1% (p = 0.002), or very low 
+
+# It's apparent the data is not normally distributed, so lets see which way the data is skewed
+wellingborough %>%
+  dplyr::select(k046)%>%
+  pastecs::stat.desc(basic = FALSE, desc = FALSE, norm = TRUE)
+
+# Distribution is skewed towards the left 
+  # SE higher than 1.29
+    # Significant result 
+# Distribution is heavy tailed
+  # High concentration of values at one end 
+      # But the 2SE falls within the range on -1.29 + 1.29 (0.75), so the result isn't significant?? 
+  # Data not skewed 
+
+
+# Relationship between some of these variables
+  # If an OA has increased households in detached house, 
+  # do those same areas also have more household with two or cars or vans 
